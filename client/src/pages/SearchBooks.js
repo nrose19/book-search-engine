3fbdb115da84385281fd13a2b354469685a5cea8
Nicole Cohen <nicolesoccer9@comcast.net>
2023-03-11 14:58:08 -0800

    addition of queries.js and mutation.js + addition of node modules

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -70,7 +70,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const {data} = await saveBook({
+      const {data} = await saveBooks({
         variables: { input: bookToSave },
       });
       // if book successfully saves to user's account, save book id to state
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,63 @@
+import { gql } from '@apollo/client';
+
+export const USER_LOGIN = gql `
+    mutation login($email: String!, $password: String!){
+        login(email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOKS = gql `
+    mutation saveBooks($input: savedBook!){
+        saveBooks(input: $input){
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                author
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOKS = gql `
+    mutation removeBook($bookId: ID!){
+        removeBook(bookId: $bookId){
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                bookId
+                title
+                author
+                description
+                image
+                link
+            }
+        }
+    }
+`
